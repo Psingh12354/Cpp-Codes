ab2281eed0de0f1df02ac9bb9a32f27e424e3c5d
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-09-06 11:04:43 +0530

    Add files via upload

---
+++ Timepass2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d;
+	cout<<"Enter the value of a and b : \n";
+	cin>>a>>b;
+	cout<<"\nEnter the value of c and d : \n";
+	cin>>c>>d;
+	a=10;
+	b=10;
+	c=10;
+	d=10;
+	int sum;
+	sum=a+b;
+	int sub;
+	sub=c-d;
+	cout<<"Answer is : "<<sum+sub;
+	return 0;
+}
