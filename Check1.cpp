952a1215fc6de72a092788e19f9f83c3b8a5b43d
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-11-24 14:37:19 +0530

    Add files via upload

---
+++ Check1.cpp
@@ -0,0 +1,16 @@
+    #include<iostream>
+    using namespace std;
+    int main()
+    {
+    	int x = -1;
+        unsigned int y = 2;
+ 
+        if(x > y) 
+        {
+        	cout << "x is greater";
+    	}
+        else 
+        {
+    		cout << "y is greater";
+    	}      
+    }
