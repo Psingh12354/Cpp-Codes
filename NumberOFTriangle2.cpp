dcb03541ebe57684fc8d8c5bc74899925b3eef0a
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-11-28 15:04:39 +0530

    Add files via upload

---
+++ NumberOFTriangle2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	cout<<"Enter the number of bases of triangle : ";
+	cin>>n;
+	sum=(n*(n+1))/2;
+	cout<<"Number of triangle is : "<<sum;
+	return 0;	
+}
