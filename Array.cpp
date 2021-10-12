b313610218758fcb7faddbe97cf8d8ff7413c0d9
sagar kumar <61751959+18bet@users.noreply.github.com>
2021-10-12 17:27:24 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	int arr[3]={43,54,54};
-	cout<<arr[0]<<endl; //Here we have done this program to know the element at given position
+	cout<<arr[0];//Here we have done this program to know the element at given position
+	cout<<endl;
 	return 0;
 }
